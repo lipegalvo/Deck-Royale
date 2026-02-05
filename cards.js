da4460841834cf42ca0e57750c1dde4759f04e97
lipegalvo <lipe45548@gmail.com>
2026-02-05 10:44:59 -0300

    Create cards.js
    Signed-off-by: lipegalvo <lipe45548@gmail.com>

---
+++ cards.js
@@ -0,0 +1,18 @@
+const todasCartas = [
+  { nome: "Cavaleiro", tipo: "tropa", elixir: 3 },
+  { nome: "Arqueiras", tipo: "tropa", elixir: 3 },
+  { nome: "Gigante", tipo: "tanque", elixir: 5 },
+  { nome: "Mago", tipo: "suporte", elixir: 5 },
+  { nome: "Bola de Fogo", tipo: "feitiço", elixir: 4 },
+  { nome: "Zap", tipo: "feitiço", elixir: 2 },
+  { nome: "Mosqueteira", tipo: "suporte", elixir: 4 },
+  { nome: "Mini P.E.K.K.A", tipo: "dano", elixir: 4 },
+  { nome: "Corredor", tipo: "win", elixir: 4 },
+  { nome: "Valquíria", tipo: "tropa", elixir: 4 },
+  { nome: "Esqueletos", tipo: "ciclo", elixir: 1 },
+  { nome: "Canhão", tipo: "defesa", elixir: 3 },
+  { nome: "Dragão Infernal", tipo: "dano", elixir: 4 },
+  { nome: "P.E.K.K.A", tipo: "tanque", elixir: 7 },
+  { nome: "Balão", tipo: "win", elixir: 5 },
+  { nome: "Príncipe", tipo: "dano", elixir: 5 }
+];
